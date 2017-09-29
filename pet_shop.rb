ca98101f4fd0627b0b908be26265d7f6221fc59d
Peter Crowther <petercrowther90@hotmail.com>
2017-09-29 13:33:40 +0100

    six tests passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -11,9 +11,13 @@ def add_or_remove_cash(pet_shop, amount)
 end
 
 def add_or_remove_cash(pet_shop, amount)
-  pet_shop [:admin][:total_cash] += amount
+  pet_shop[:admin][:total_cash] += amount
 end
 
 def pets_sold(sales)
   return sales[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, amount)
+  pet_shop[:admin][:pets_sold] += amount
 end
